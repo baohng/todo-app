73e83601a3c648a7cc1ae19a78ea9ce4c3191ff5
baohng <bao.hng221b@gmail.com>
2023-09-09 09:17:18 +0700

    add new package for component

---
+++ todo-list-frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+export default function Header() {
+  return (
+      <header>
+        <h1>
+          <HighlightIcon />
+          Todo
+        </h1>
+      </header>
+  )
+}
